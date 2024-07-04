746fa4c965bc29be99d46f7a62e493bfd3ffd077
woodlc <373845395@qq.com>
2024-07-04 13:20:49 +0800

    1

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,83 @@
+module.exports = {
+    title: 'woodlc开发笔记',
+    logo:'logo.png',
+    description: 'Just playing around.',
+    themeConfig:{
+        //导航栏
+        nav:[
+            {
+                text:"php",
+                items:[
+                    {text:'入门',link:'/php/入门'},
+                ]
+            },
+            {text:"前端",link:'/前端/介绍'},
+            {
+                text:"关于",
+                items:[
+                    {text:'Gitee',link:'https://gitee.com/woodlc'},
+                ]
+            }
+        ],
+        //搜索
+        search: {
+            provider: 'local'
+        },
+        //社交属性
+        // socialLinks: [
+            // { icon: 'github', link: 'https://gitee.com/woodlc' },
+            // { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+            // { icon: 'twitter', link: '...' },
+            // 也可以通过自定义的方式
+            // {
+            //   icon: {
+            //     svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Dribbble</title><path d="M12...6.38z"/></svg>'
+            //   },
+            //   link: '...'
+            // }
+        //   ],
+          footer: {
+            message: 'woodlc',
+            copyright: ' © 2024-vitepress'
+          },
+          //侧边栏
+          sidebar: [
+            {
+                text: "欢迎阅读",
+                items: [
+                  {
+                    text: "阅读须知",
+                    link: "/posts/阅读须知",
+                  },          
+                ],
+              },
+              {
+                text: "vitepress",
+                collapsible:true,
+                collapsed:true,
+                items: [
+                  {
+                    text: "介绍",
+                    link: "/posts/vitepress/介绍",
+                  },
+                  {
+                    text:"快速上手",
+                    link:"/posts/vitepress/快速上手"
+                  },
+                  {
+                    text:"主页",
+                    link:"/posts/vitepress/主页"
+                  },
+                  {
+                    text:"导航栏",
+                    link:"/posts/vitepress/导航栏"
+                  },
+                  {
+                    text:"侧边栏",
+                    link:"/posts/vitepress/侧边栏"
+                  }
+                ],
+              }
+          ]
+    }
+  }
